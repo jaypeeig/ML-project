6fc981669dadfdad77d507ecc26340b0e9b6fc5c
jaypeeig <jaypeeig@gmail.com>
2017-10-16 14:25:19 +0800

    Add solution for problem 2

---
+++ problem2/prob2-js.js
@@ -0,0 +1,15 @@
+/*
+	https://projecteuler.net/problem=2
+*/
+
+let fib = [1,2];
+
+for(let i=2; i <= 31; i++)
+{
+    fib[i] = fib[i-2] + fib[i-1];
+}
+
+const fibFilt = fib.filter((num) => num % 2 == 0);
+const fibSum = fibFilt.reduce((acc,val) => acc + val);
+
+console.log(fibSum);
