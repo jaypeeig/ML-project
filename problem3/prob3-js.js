f457e5415be3dbe7a45aad3f9e352a1c54ac80cb
jaypeeig <jaypeeig@gmail.com>
2017-10-16 16:26:50 +0800

    Add solution to problem 3

--- problem3/prob3-js.js
+++ problem3/prob3-js.js
@@ -3,3 +3,25 @@
 */
 
 const num = 600851475143;
+var foo = 0;
+
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num !== 1;
+}
+
+
+for (let i = 2; i <= num; i++) {
+	if ((num % i == 0)){
+		if (isPrime(i)) {
+			foo = i;
+		}else{
+			break;
+		}
+	}
+}
+
+console.log(foo);
+
+
