493f2b209711ecac917b09a11cebc5e5e2340a27
jaypeeig <jaypeeig@gmail.com>
2017-10-16 14:30:29 +0800

    Update code in one line operation

--- problem2/prob2-js.js
+++ problem2/prob2-js.js
@@ -9,7 +9,6 @@ for(let i=2; i <= 31; i++)
     fib[i] = fib[i-2] + fib[i-1];
 }
 
-const fibFilt = fib.filter((num) => num % 2 == 0);
-const fibSum = fibFilt.reduce((acc,val) => acc + val);
+const fibFilt = fib.filter((num) => num % 2 == 0).reduce((acc,val) => acc + val);
 
-console.log(fibSum);
+console.log(fibFilt);
